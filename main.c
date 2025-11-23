a2663bb8ba4964516e938b05bcdfd8b8e3e272de
Rithvik <84326832+rithvikr9@users.noreply.github.com>
2025-11-23 10:07:56 +0530

    chore: code cleanup

--- main.c
+++ main.c
@@ -10,18 +10,18 @@ bool check_if_process(char *);
 FILE *get_stat_file(char *);
 
 int main() {
-  DIR* pdir = opendir("/proc");
-  if (pdir == NULL) return 1;
+  DIR* proc = opendir("/proc");
+  if (proc == NULL) return 1;
 
-  struct dirent* nextpdir = readdir(pdir);
+  struct dirent* nextproc = readdir(proc);
 
   // print header
   printf("%6s  %-40s %-6s %8s %10s %10s %8s %15s %12s\n", 
     "PID", "NAME", "STATE", "PPID", "UTIME", "STIME", "NICE", "VSIZE", "RSS");
   printf("----------------------------------------------------------------------------------------------------------------------------\n");
 
-  while (nextpdir != NULL) {
-    char *dir_name = nextpdir->d_name;
+  while (nextproc != NULL) {
+    char *dir_name = nextproc->d_name;
 
     if (check_if_process(dir_name)) {
       char stat_str[4096];
@@ -29,7 +29,7 @@ int main() {
 
       FILE* statfile = get_stat_file(dir_name);
       if (statfile == NULL) {
-        nextpdir = readdir(pdir);
+        nextproc = readdir(proc);
         continue;
       }
 
@@ -43,41 +43,41 @@ int main() {
       fclose(statfile);
     }
 
-    nextpdir = readdir(pdir);
+    nextproc = readdir(proc);
   }
 
-  closedir(pdir);
+  closedir(proc);
 }
 
 void tokenize_data(char *stat_str, char **fields) {
-      char *first_open_bracket = strchr(stat_str, '(');
-      char *last_close_bracket = strrchr(stat_str, ')');
-      if (first_open_bracket == NULL || last_close_bracket == NULL) return;
+  char *first_open_bracket = strchr(stat_str, '(');
+  char *last_close_bracket = strrchr(stat_str, ')');
+  if (first_open_bracket == NULL || last_close_bracket == NULL) return;
 
 
-      // manually put delimiter before and after the parentheses
-      *first_open_bracket = '\0';
-      *last_close_bracket = '\0';
+  // manually put delimiter before and after the parentheses
+  *first_open_bracket = '\0';
+  *last_close_bracket = '\0';
 
-      // set first and second elements to pid and process name
-      fields[0] = stat_str;
-      fields[1] = first_open_bracket + 1;
+  // set first and second elements to pid and process name
+  fields[0] = stat_str;
+  fields[1] = first_open_bracket + 1;
 
-      char *remaining_data = last_close_bracket + 2;
+  char *remaining_data = last_close_bracket + 2;
 
-      char *token = strtok(remaining_data, " ");
+  char *token = strtok(remaining_data, " ");
 
-      int i = 2;
-      while (token != NULL && i < 52) {
-        fields[i++] = token;
-        token = strtok(NULL, " ");
-      }
+  int i = 2;
+  while (token != NULL && i < 52) {
+    fields[i++] = token;
+    token = strtok(NULL, " ");
+  }
 }
 
 bool check_if_process(char *dir_name) {
   int i = 0;
   while (dir_name[i] != '\0') {
-    if (!isdigit(*(dir_name + i))) return false;
+    if (!isdigit(dir_name[i])) return false;
     i++;
   }
 
