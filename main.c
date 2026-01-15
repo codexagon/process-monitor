431f2861cab4d83547f6a1a5c0ce3b91eef8246e
Rithvik <84326832+rithvikr9@users.noreply.github.com>
2026-01-15 13:06:52 +0530

    refactor: put terminal setup & restoring in separate functions

--- main.c
+++ main.c
@@ -10,33 +10,22 @@
 #include "display.h"
 #include "process.h"
 
+int setup_terminal(struct termios *original_config, struct winsize *win);
+int restore_terminal(struct termios *original_config);
+
 int main() {
 	bool running = true;
 
 	struct winsize win;
 	struct termios original_config;
-	tcgetattr(STDIN_FILENO, &original_config);
-	struct termios raw_config = original_config;
 
-	// disable canonical mode & input echoing
-	raw_config.c_lflag &= ~(ICANON | ECHO);
-	tcsetattr(STDIN_FILENO, TCSANOW, &raw_config);
+	if (setup_terminal(&original_config, &win) != 0)
+		return 1;
 
-	// make non-blocking
-	int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
-	fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
-
-	int success = ioctl(STDOUT_FILENO, TIOCGWINSZ, &win);
-	if (success != 0)
-		return 2;
+	fflush(stdout);
 
 	int start_process = 0;
 	int max_processes = win.ws_row - 2;
-
-	printf("\033[?25l");     // hide cursor
-	printf("\033[?1049h");   // enter alternate buffer
-	printf("\033[2J\033[H"); // clear screen and move cursor to home
-	fflush(stdout);
 
 	display_header(win.ws_col);
 
@@ -101,9 +90,36 @@ int main() {
 
 	free(processes.processes);
 
-	tcsetattr(STDIN_FILENO, TCSANOW, &original_config);
+	restore_terminal(&original_config);
+	fflush(stdout);
+}
+
+int setup_terminal(struct termios *original_config, struct winsize *win) {
+	tcgetattr(STDIN_FILENO, original_config);
+	struct termios raw_config = *original_config;
+
+	raw_config.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &raw_config);
+
+	int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
+	fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
+
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, win) != 0)
+		return 2;
+
+	printf("\033[?25l");     // hide cursor
+	printf("\033[?1049h");   // enter alternate buffer
+	printf("\033[2J\033[H"); // clear screen and move cursor to home
+
+	return 0;
+}
+
+int restore_terminal(struct termios *original_config) {
+	tcsetattr(STDIN_FILENO, TCSANOW, original_config);
+
 	printf("\033[2J\033[H"); // clear screen, move cursor to home
 	printf("\033[?1049l");   // exit alternate buffer
 	printf("\033[?25h");     // show cursor
-	fflush(stdout);
+	
+	return 0;
 }
